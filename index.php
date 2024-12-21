5913055b4ef10a3b01fbdeba2b9aa23abd4ba5ae
thedarkking01 <sourabhsinghbais52@gmail.com>
2024-12-21 16:27:34 +0530

    Project file and folder setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Q&A platform</title>
+    <?php include('./client/commonFiles.php') ?>
+</head>
+<body>
+    <?php include('./client/header.php')?>
+</body>
+</html>
